7dd1ca7a118c6444edf60a77cf3cba5131fa6ff2
alison-lake5media <49420721+AlisonAspen@users.noreply.github.com>
2022-08-24 14:21:40 -0400

    solutions page template

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+function toggleActive(i) {
+  if(i == 0) {
+    console.log("MediaSpectrum");
+  }
+  if(i == 1) {
+    console.log("tacticvision");
+  }
+  if(i == 2) {
+    console.log("MediaFrontier");
+  }
+}
