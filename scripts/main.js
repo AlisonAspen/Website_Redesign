217e5c820d53268ae1f59213750d0c7990568fa2
alison-lake5media <49420721+AlisonAspen@users.noreply.github.com>
2022-08-25 16:13:53 -0400

    solutions navigation management

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,37 @@
-function toggleActive(i) {
-  if(i == 0) {
-    console.log("MediaSpectrum");
-  }
-  if(i == 1) {
-    console.log("tacticvision");
-  }
-  if(i == 2) {
-    console.log("MediaFrontier");
-  }
+function showSolutions() {
+  $(".mediaspectrum").css("display", "block");
+  console.log("solutions page loaded");
 }
+
+$('#mediaspectrum').click(function() {
+  $(this).css("text-decoration", "underline");
+  $(".ms2-text").css("display", "block");
+
+  $(".ttv-text").css("display", "none");
+  $("#tacticvision").css("text-decoration", "none");
+  $(".mft-text").css("display", "none");
+  $("#mediafrontier").css("text-decoration", "none");
+  console.log("mediaspectrum clicked");
+});
+
+$('#tacticvision').click(function() {
+  $(this).css("text-decoration", "underline");
+  $(".ttv-text").css("display", "block");
+
+  $(".ms2-text").css("display", "none");
+  $("#mediaspectrum").css("text-decoration", "none");
+  $(".mft-text").css("display", "none");
+  $("#mediafrontier").css("text-decoration", "none");
+  console.log("ttvclicked");
+});
+
+$('#mediafrontier').click(function() {
+  $(this).css("text-decoration", "underline");
+  $(".mft-text").css("display", "block");
+
+  $(".ttv-text").css("display", "none");
+  $("#tacticvision").css("text-decoration", "none");
+  $(".ms2-text").css("display", "none");
+  $("#mediaspectrum").css("text-decoration", "none");
+  console.log("mft clicked");
+});
